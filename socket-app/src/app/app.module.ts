97d26562f3b1a95159ce1ee1116da545b1627288
Jake Shelton <jake.s.shelton@gmail.com>
2019-07-27 14:39:28 -0600

    add document-list component

--- socket-app/src/app/app.module.ts
+++ socket-app/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { DocumentListComponent } from './document-list/document-list.component';
 import { DocumentComponent } from './document/document.component';
 
+// Socket.io imports
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:1234', options: {} };
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,9 @@ import { DocumentComponent } from './document/document.component';
     DocumentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
